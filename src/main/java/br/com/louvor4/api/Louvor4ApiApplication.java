b6275bfab770f12a0a03a87742f44b86e2dafd6e
Erivaldo Lourenco <erivaldo.mariano@pdcase.com.br>
2025-05-23 14:32:45 -0300

    Iniciando projeto.

---
+++ src/main/java/br/com/louvor4/api/Louvor4ApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.louvor4.api;
+
+import org.springframework.boot .SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Louvor4ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Louvor4ApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/louvor4/api/Louvor4ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.louvor4.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Louvor4ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

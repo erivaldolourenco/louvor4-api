5de1bc0dfbc438277db1208de8e04b722a4fa22e
erivaldolourenco <35848782+erivaldolourenco@users.noreply.github.com>
2025-07-01 10:26:33 -0300

    Fix SpringApplication import

--- src/main/java/br/com/louvor4/api/Louvor4ApiApplication.java
+++ src/main/java/br/com/louvor4/api/Louvor4ApiApplication.java
@@ -1,6 +1,6 @@
 package br.com.louvor4.api;
 
-import org.springframework.boot .SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication

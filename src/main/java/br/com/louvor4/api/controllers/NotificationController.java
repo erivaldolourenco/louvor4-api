a35dee7cd5ce08d0413c0b7717a42c1f4e4953b9
erivaldo <erivaldolourenco.m@gmail.com>
2026-03-31 23:39:02 -0300

    Adiciona meto para marcar notificacao de uusuario como read.

--- src/main/java/br/com/louvor4/api/controllers/NotificationController.java
+++ src/main/java/br/com/louvor4/api/controllers/NotificationController.java
@@ -1,11 +1,14 @@
 package br.com.louvor4.api.controllers;
 
+import br.com.louvor4.api.config.security.CurrentUserProvider;
 import br.com.louvor4.api.services.NotificationDeviceService;
-import br.com.louvor4.api.services.PushSenderService;
+import br.com.louvor4.api.services.UserNotificationService;
 import br.com.louvor4.api.shared.dto.notification.RegisterDeviceRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 
 @RestController
@@ -13,14 +16,29 @@ import org.springframework.web.bind.annotation.*;
 public class NotificationController {
 
     private final NotificationDeviceService deviceService;
+    private final UserNotificationService userNotificationService;
+    private final CurrentUserProvider currentUserProvider;
 
-    public NotificationController(NotificationDeviceService deviceService) {
+    public NotificationController(
+            NotificationDeviceService deviceService,
+            UserNotificationService userNotificationService,
+            CurrentUserProvider currentUserProvider
+    ) {
         this.deviceService = deviceService;
+        this.userNotificationService = userNotificationService;
+        this.currentUserProvider = currentUserProvider;
     }
 
     @PostMapping("/device-register")
     public ResponseEntity<Void> deviceRegister(@RequestBody @Valid RegisterDeviceRequest req) {
         deviceService.registerDevice(req);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{notificationId}/read")
+    public ResponseEntity<Void> notificationRead(@PathVariable UUID notificationId){
+        var user = currentUserProvider.get();
+        userNotificationService.markAsRead(user.getId(), notificationId);
+        return ResponseEntity.noContent().build();
     }
 }
